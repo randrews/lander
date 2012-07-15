f0dd0fd1e76cd73935dfad36d7eac80c4440ec14
Ross Andrews <randrews@geekfu.org>
2012-07-15 16:05:51 -0500

    Physics engine, thrust actually moves rocket.

--- main.lua
+++ main.lua
@@ -5,9 +5,10 @@ local background = nil
 local dialogFont = nil
 local game = nil
 local status_bar = nil
-local rocket = nil
+local rocket_imq = nil
 local particle = nil
 local thrust = nil
+local world = nil
 
 local rocket_pos = {
    x = 400,
@@ -17,7 +18,7 @@ local rocket_pos = {
 
 function love.load()
    math.randomseed(os.time())
-   rocket = love.graphics.newImage('rocket.png')
+   rocket_img = love.graphics.newImage('rocket.png')
    background = love.graphics.newImage('background.jpg')
    particle = love.graphics.newImage('particle.png')
 
@@ -42,6 +43,12 @@ function love.load()
 
    thrust:start()
    -- game = create_game()
+
+   love.physics.setMeter(64)
+   world = love.physics.newWorld(0, 10)
+   rocket = love.physics.newBody(world, 400, 300, 'dynamic')
+   rocket_shape = love.physics.newRectangleShape(1, 1)
+   fixture = love.physics.newFixture(rocket, rocket_shape)
 end
 
 function love.draw()
@@ -49,9 +56,9 @@ function love.draw()
    g.draw(background, 0, 0)
 
    g.draw(thrust, 0, 0)
-   g.draw(rocket,
-          rocket_pos.x, rocket_pos.y, -- position
-          rocket_pos.a, -- rotation
+   g.draw(rocket_img,
+          rocket:getX(), rocket:getY(), -- position
+          rocket:getAngle(), -- rotation
           1, 1, -- scale
           32, 32) -- Offset to image center
 end
@@ -63,19 +70,23 @@ function love.mousereleased()
 end
 
 function love.update(dt)
+   local dx = love.mouse.getX() - rocket:getX()
+   local dy = love.mouse.getY() - rocket:getY()
+
+
+   local a = math.atan2(dy, dx)
+   rocket:setAngle(a + math.pi/2) -- / math.pi * 180
+
+   thrust:setPosition(rocket:getX() - math.cos(a)*32, rocket:getY() - math.sin(a)*32)
+   thrust:setDirection(a)
+
    if love.mouse.isDown('l') then
       thrust:setEmissionRate(100)
+      rocket:applyForce(math.cos(a)*0.01, math.sin(a)*0.01)
    else
       thrust:setEmissionRate(0)
    end
 
-   local dx = love.mouse.getX() - rocket_pos.x
-   local dy = love.mouse.getY() - rocket_pos.y
-
-   local a = math.atan2(dy, dx)
-   rocket_pos.a = a + math.pi/2 -- / math.pi * 180
-
-   thrust:setPosition(rocket_pos.x - math.cos(a)*32, rocket_pos.y - math.sin(a)*32)
-   thrust:setDirection(a)
    thrust:update(dt)
+   world:update(dt)
 end
