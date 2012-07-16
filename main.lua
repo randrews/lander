6995859360bba1653f1b3969b3dd9e38d8c5ab73
Ross Andrews <randrews@geekfu.org>
2012-07-15 22:20:57 -0500

    Borders around world.

--- main.lua
+++ main.lua
@@ -5,20 +5,17 @@ local background = nil
 local dialogFont = nil
 local game = nil
 local status_bar = nil
-local rocket_imq = nil
+local rocket_img = nil
 local particle = nil
 local thrust = nil
 local world = nil
 
-local rocket_pos = {
-   x = 400,
-   y = 300,
-   a = 0
-}
+local edge = {}
+local rocket = {}
 
 function love.load()
    math.randomseed(os.time())
-   rocket_img = love.graphics.newImage('rocket.png')
+   rocket.image = love.graphics.newImage('rocket.png')
    background = love.graphics.newImage('background.jpg')
    particle = love.graphics.newImage('particle.png')
 
@@ -38,7 +35,7 @@ function love.load()
    thrust:setSizes(4, 1)
    thrust:setSizeVariation(1)
    thrust:setSpeed(-150)
-   --thrust:setSpread(math.pi / 9)
+   thrust:setSpread(math.pi / 9)
    thrust:setTangentialAcceleration(-100, 100)
 
    thrust:start()
@@ -46,9 +43,21 @@ function love.load()
 
    love.physics.setMeter(64)
    world = love.physics.newWorld(0, 10)
-   rocket = love.physics.newBody(world, 400, 300, 'dynamic')
-   rocket_shape = love.physics.newRectangleShape(1, 1)
-   fixture = love.physics.newFixture(rocket, rocket_shape)
+
+   rocket.body = love.physics.newBody(world, 400, 300, 'dynamic')
+   rocket.shape = love.physics.newRectangleShape(64, 64)
+   love.physics.newFixture(rocket.body, rocket.shape)
+
+   edge.body = love.physics.newBody(world, 0, 0, 'static')
+   edge.shapes = {}
+   table.insert(edge.shapes, love.physics.newEdgeShape(0, 600, 800, 600))
+   table.insert(edge.shapes, love.physics.newEdgeShape(0, 0, 800, 0))
+   table.insert(edge.shapes, love.physics.newEdgeShape(0, 0, 0, 600))
+   table.insert(edge.shapes, love.physics.newEdgeShape(800, 0, 800, 600))
+
+   for _, s in ipairs(edge.shapes) do
+      love.physics.newFixture(edge.body, s)
+   end
 end
 
 function love.draw()
@@ -56,9 +65,9 @@ function love.draw()
    g.draw(background, 0, 0)
 
    g.draw(thrust, 0, 0)
-   g.draw(rocket_img,
-          rocket:getX(), rocket:getY(), -- position
-          rocket:getAngle(), -- rotation
+   g.draw(rocket.image,
+          rocket.body:getX(), rocket.body:getY(), -- position
+          rocket.body:getAngle(), -- rotation
           1, 1, -- scale
           32, 32) -- Offset to image center
 end
@@ -70,19 +79,20 @@ function love.mousereleased()
 end
 
 function love.update(dt)
-   local dx = love.mouse.getX() - rocket:getX()
-   local dy = love.mouse.getY() - rocket:getY()
+   local dx = love.mouse.getX() - rocket.body:getX()
+   local dy = love.mouse.getY() - rocket.body:getY()
 
 
    local a = math.atan2(dy, dx)
-   rocket:setAngle(a + math.pi/2) -- / math.pi * 180
+   rocket.body:setAngle(a + math.pi/2) -- / math.pi * 180
 
-   thrust:setPosition(rocket:getX() - math.cos(a)*32, rocket:getY() - math.sin(a)*32)
+   thrust:setPosition(rocket.body:getX() - math.cos(a)*32, rocket.body:getY() - math.sin(a)*32)
    thrust:setDirection(a)
 
    if love.mouse.isDown('l') then
       thrust:setEmissionRate(100)
-      rocket:applyForce(math.cos(a)*0.01, math.sin(a)*0.01)
+      local pwr = 25
+      rocket.body:applyForce(math.cos(a)*pwr, math.sin(a)*pwr)
    else
       thrust:setEmissionRate(0)
    end
